6d4e99550c315da06aebf4406394bd7e1f9b3bf0
Tetyana Kuziv <kuziv2000@gmail.com>
2017-11-11 14:08:20 +0200

    implemented task 2(begin)

---
+++ ConsoleApp2begin/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2begin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the side a");
+            string s = Console.ReadLine();
+            try
+            {
+                int a = Int32.Parse(s);
+                if (a <= 0)
+                {
+                    Console.WriteLine("Please enter another number");
+                    Console.ReadKey();
+                    return;
+                }
+                int S = a*a;
+                Console.WriteLine("Result: {0}", S);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
